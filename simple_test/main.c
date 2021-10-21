cffe78d6ab9e63b1f82dc3b02524e33123f22f8e
majay123 <836584760@qq.com>
2021-10-21 10:53:31 +0800

    up something

--- simple_test/main.c
+++ simple_test/main.c
@@ -31,7 +31,7 @@
  * @Author       : MCD
  * @Date         : 2021-10-13 09:17:27
  * @LastEditors  : MCD
- * @LastEditTime : 2021-10-16 13:29:42
+ * @LastEditTime : 2021-10-20 13:54:31
  * @FilePath     : /My_C_Test/simple_test/main.c
  * @Description  : 
  * 
@@ -42,6 +42,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 // #include "common.h"
 
 static uint32_t _hex_to_uint(uint8_t *hex, size_t len)
@@ -63,7 +64,7 @@ static void _uint_to_hex(uint32_t num, uint8_t *data, size_t len)
     int i = len - 1;
     for (; i >= 0; i--) {
         data[i] = (uint8_t)((num >> (i * 8)) & 0xFF);
-        printf("(%ld)%02x ",i,  data[i]);
+        printf("(%ld)%02x ", i, data[i]);
     }
     printf("\n");
 }
@@ -92,20 +93,125 @@ int my_itoa(uint8_t *buf, uint32_t value, size_t size)
     return 0;
 }
 
+void cancelchar(char *a, char n)
+{
+    char *p;
+    for (p = a; *p != '\0'; p++)
+        if (*p != n)
+            *a++ = *p;
+    *a = '\0';
+}
+
+void del_sp(char *src, char n) // 删除C风格字符串中的空格
+{
+    char *fp = src;
+    while (*src) {
+        if (*src != n) { // 如果不是空格就复制
+            *fp = *src;
+            fp++;
+        }
+        src++;
+    }
+    *fp = '\0'; //封闭字符串
+}
+
+void split(char *str, char *delims)
+{
+    char *result = NULL;
+    result = strtok(str, delims);
+    while (result != NULL) {
+        printf("result is \"%s\", %d\n", result, atoi(result));
+        result = strtok(NULL, delims);
+    }
+}
+
+
+
+
+int hexStr2bytes(const uint8_t *hexStr, uint8_t *buf, int bufLen)
+{
+    int i;
+    int len;
+
+    if (NULL == hexStr) {
+        len = 0;
+    } else {
+        len = (int)strlen(hexStr) / 2;
+
+        if (bufLen < len) {
+            len = bufLen;
+        }
+    }
+
+    for (i = 0; i < len; i++) {
+        char ch1, ch2;
+        int val;
+
+        ch1 = hexStr[i * 2];
+        ch2 = hexStr[i * 2 + 1];
+        if (ch1 >= '0' && ch1 <= '9') {
+            val = (ch1 - '0') * 16;
+        } else if (ch1 >= 'a' && ch1 <= 'f') {
+            val = ((ch1 - 'a') + 10) * 16;
+        } else if (ch1 >= 'A' && ch1 <= 'F') {
+            val = ((ch1 - 'A') + 10) * 16;
+        } else {
+            return -1;
+        }
+
+        if (ch2 >= '0' && ch2 <= '9') {
+            val += ch2 - '0';
+        } else if (ch2 >= 'a' && ch2 <= 'f') {
+            val += (ch2 - 'a') + 10;
+        } else if (ch2 >= 'A' && ch2 <= 'F') {
+            val += (ch2 - 'A') + 10;
+        } else {
+            return -1;
+        }
+
+        buf[i] = val & 0xff;
+    }
+
+    return 0;
+}
+
+int get_addr(char *src_addr, char *dst_addr, size_t len)
+{
+    del_sp(src_addr, ':');
+    hexStr2bytes(src_addr, dst_addr, len);
+}
+
 int main(int argc, char const *argv[])
 {
-    uint8_t time[4] = {0x00, 0x01, 0xe0, 0x19};
+    uint8_t time1[4] = {0x00, 0x01, 0xe0, 0x19};
     uint8_t data[4] = {0};
     uint8_t data1[4] = {0};
+    char test1[20] = "00:E0:99:13:C7:62";
+    uint8_t buf[20] = {0};
     _Bool test = 0;
     // char *end;
     uint32_t running_time = 10 * 1000;
+
+    // del_sp(test1, ':');
+    // printf("%s \n", test1);
+    // hexStr2bytes(test1, buf, 8);
+    get_addr(test1, buf, 8);
+    for (size_t i = 0; i < 8; i++)
+    {
+        printf("%02X ", buf[i]);
+    }
+    printf("\n");
+    
+    time_t t;
+    srand((unsigned) time(&t));
+    printf("%d %d\n", rand() % 256, rand() % 256);
+    
     // running_time = _hex_to_uint(time, sizeof(time));
     // printf("Running time: %d\n", running_time);
     // _uint_to_hex(running_time, data, sizeof(data));
-    printf("Running time: %x, %ld\n", running_time, running_time);
-    my_itoa(data1, running_time, sizeof(data1));
-    printf("%02x %02x %02x %02x\n", data1[0], data1[1], data1[2], data1[3]);
+    // printf("Running time: %x, %ld\n", running_time, running_time);
+    // my_itoa(data1, running_time, sizeof(data1));
+    // printf("%02x %02x %02x %02x\n", data1[0], data1[1], data1[2], data1[3]);
     // uint8_t mac_addr[8] = {0xF0, 0x0A, 0x4F, 0x04, 0x10, 0x2D, 0x01, 0x01};
     // uint8_t dest_addr[25] = {0};
     // hextodecstring(mac_addr, sizeof(mac_addr), dest_addr);
